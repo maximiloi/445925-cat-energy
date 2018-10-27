29d00ed3e70f1d7094168edc00b56ffc8031985c
maximiloi <maximiloi@gmail.com>
2018-10-27 23:21:18 +0300

    исправления6

--- gulpfile.js
+++ gulpfile.js
@@ -80,15 +80,14 @@ gulp.task("clean", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+gulp.watch("source/less/**/*.less", gulp.series("css")); gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+gulp.watch("source/*.html", gulp.series("html", "refresh")); });
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
 });
 
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
